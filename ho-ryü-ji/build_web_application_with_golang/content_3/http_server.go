e2e598db15e5389363142ffcc9f6da95e5e1c1f9
ho-ryu-ji <ryuta.soc7@gmail.com>
2022-05-11 23:46:13 +0900

    created login.go

--- ho-ryü-ji/build_web_application_with_golang/content_3/http_server.go
+++ ho-ryü-ji/build_web_application_with_golang/content_3/http_server.go
@@ -19,6 +19,36 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello astaxie!")
 }
 
+// func (srv *Server) Serve(l net.Listener) error {
+// 	defer l.Close()
+// 	var tempDelay time.Duration
+// 	for {
+// 		rw, e := l.Accept()
+// 		if e != nil {
+// 			if ne, ok := e.(net.Error); ok && ne.Temporary() {
+// 				if tempDelay == 0 {
+// 					tempDelay = 5 * time.Millisecond
+// 				} else {
+// 					tempDelay *= 2
+// 				}
+// 				if max := 1 * time.Second; tempDelay > max {
+// 					tempDelay = max
+// 				}
+// 				log.Printf("http: Accept error: %v; retrying in %v", e, tempDelay)
+// 				time.Sleep(tempDelay)
+// 				continue
+// 			}
+// 			return e
+// 		}
+// 		tempDelay = 0
+// 		c, err := srv.newConn(rw)
+// 		if err != nil {
+// 			continue
+// 		}
+// 		go c.serve()
+// 	}
+// }
+
 func main() {
 	http.HandleFunc("/", sayHelloName)
 	err := http.ListenAndServe(":9090", nil)
