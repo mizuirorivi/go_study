893c14ed9a471faab4e46832fdcfc7cdd95bf2a1
mizuirorivi <onodaha@gmail.com>
2022-05-24 14:05:41 +0900

    モジュールのテストの勉強

---
+++ mizuiro/go_module_test/go.mod
@@ -0,0 +1,5 @@
+module tekitou
+
+go 1.18
+
+require github.com/mizuirorivi/go_module_test v0.0.0-20220524043431-172ef9667e09
---
+++ mizuiro/go_module_test/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mizuirorivi/go_module_test"
+
+func main() {
+	go_module_test.Print_mizuiro()
+}
