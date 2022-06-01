d1e972fd3435880549cec4124fe50729952cc4e1
mizuirorivi <onodaha@gmail.com>
2022-06-01 14:07:36 +0900

    session login の作成

--- ho-ryü-ji/build_web_application_with_golang/content_6/session_study/router/login.go
+++ ho-ryü-ji/build_web_application_with_golang/content_6/session_study/router/login.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,25 +10,31 @@ import (
 	"github.com/ho-ryue-ji/session_study/db"
 )
 
+func ViewLogin(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("pages/login.gtpl")
+	t.Execute(w, nil)
+}
 func Login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
 	if r.Method == "GET" {
-		sessionid, err := r.Cookie("sessionid")
-		if err != nil {
-			log.Fatal(err)
-		}
-		user, err := db.Get(sessionid.Value)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if user.SessionId == sessionid.Value {
+
+		sessionid, _ := r.Cookie("sessionid")
+
+		if sessionid != nil {
+			_, err := db.Get(sessionid.Value)
+			if err != nil {
+				if err == sql.ErrNoRows {
+					http.Redirect(w, r, "/register", http.StatusFound)
+					return
+				}
+				log.Fatal(err)
+			}
+
 			t, _ := template.ParseFiles("pages/success.gtpl")
 			t.Execute(w, nil)
-		} else {
-			t, _ := template.ParseFiles("pages/login.gtpl")
-			t.Execute(w, nil)
+			return
 		}
-
+		ViewLogin(w, r)
 	} else {
 		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
